145c1260818a1837eca59a4d5441adb252acc440
Binomio <onrubia78@gmail.com>
2023-04-18 23:42:23 +0200

    chore(setup): hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const { ETHERSCAN_SEPOLIA_APIKEY, INFURA_SEPOLIA_URL, ADDRESS1, ADDRESS2 } =
+  process.env;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "sepolia",
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_SEPOLIA_APIKEY || "",
+    },
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: INFURA_SEPOLIA_URL,
+      accounts: [ADDRESS1 || "", ADDRESS2 || ""],
+    },
+  },
 };
 
 export default config;
